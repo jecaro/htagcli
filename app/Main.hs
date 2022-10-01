7194bc65c73689e49871b3818d1d42ef1c6323b1
jecaro <jeancharles.quillet@gmail.com>
2022-10-01 10:08:21 +0200

    first import

---
+++ app/Main.hs
@@ -0,0 +1,95 @@
+module Main where
+
+import Options
+import Options.Applicative (execParser)
+import Path (prjSomeBase, toFilePath)
+import Sound.HTagLib (
+    Album,
+    Artist,
+    Genre,
+    TagGetter,
+    Title,
+    TrackNumber,
+    Year,
+    albumGetter,
+    albumSetter,
+    artistGetter,
+    artistSetter,
+    genreGetter,
+    genreSetter,
+    getTags,
+    setTags,
+    titleGetter,
+    titleSetter,
+    trackNumberGetter,
+    trackNumberSetter,
+    unAlbum,
+    unArtist,
+    unGenre,
+    unTitle,
+    unTrackNumber,
+    unYear,
+    yearGetter,
+    yearSetter,
+ )
+
+data AudioTrack = AudioTrack
+    { atTitle :: !Title
+    , atArtist :: !Artist
+    , atAlbum :: !Album
+    , atGenre :: !Genre
+    , atYear :: !(Maybe Year)
+    , atTrack :: !(Maybe TrackNumber)
+    }
+    deriving (Show)
+
+render :: AudioTrack -> Text
+render AudioTrack{..} =
+    unlines
+        [ "Title: " <> unTitle atTitle
+        , "Artist: " <> unArtist atArtist
+        , "Album: " <> unAlbum atAlbum
+        , "Genre: " <> unGenre atGenre
+        , "Year: " <> withMissing unYear atYear
+        , "Track: " <> withMissing unTrackNumber atTrack
+        ]
+
+withMissing :: Show b => (a -> b) -> Maybe a -> Text
+withMissing _ Nothing = "missing"
+withMissing f (Just x) = show . f $ x
+
+audioTrackGetter :: TagGetter AudioTrack
+audioTrackGetter =
+    AudioTrack
+        <$> titleGetter
+        <*> artistGetter
+        <*> albumGetter
+        <*> genreGetter
+        <*> yearGetter
+        <*> trackNumberGetter
+
+main :: IO ()
+main = do
+    options <- execParser optionsInfo
+    case options of
+        Display DisplayOptions{..} -> do
+            let filename = prjSomeBase toFilePath doFile
+            track <- getTags filename audioTrackGetter
+            putTextLn $ render track
+        Edit EditOptions{..} -> do
+            let filename = prjSomeBase toFilePath eoFile
+                setter =
+                    fold $
+                        catMaybes
+                            [ titleSetter <$> eoTitle
+                            , artistSetter <$> eoArtist
+                            , albumSetter <$> eoAlbum
+                            , genreSetter <$> eoGenre
+                            , toSetter yearSetter eoYear
+                            , toSetter trackNumberSetter eoTrack
+                            ]
+            setTags filename Nothing setter
+  where
+    toSetter _ Nothing = Nothing
+    toSetter setter (Just Remove) = Just $ setter Nothing
+    toSetter setter (Just (Set v)) = Just . setter $ Just v
---
+++ app/Options.hs
@@ -0,0 +1,149 @@
+module Options (
+    DisplayOptions (..),
+    EditOptions (..),
+    Options (..),
+    SetOrRemove (..),
+    optionsInfo,
+) where
+
+import Options.Applicative (
+    Parser,
+    ParserInfo,
+    argument,
+    command,
+    help,
+    helper,
+    hsubparser,
+    idm,
+    info,
+    long,
+    maybeReader,
+    metavar,
+    option,
+    progDesc,
+    strOption,
+ )
+import Options.Applicative.Builder (flag')
+import Path (
+    File,
+    SomeBase,
+    parseSomeFile,
+ )
+import Sound.HTagLib (
+    Album,
+    Artist,
+    Genre,
+    Title,
+    TrackNumber,
+    Year,
+    mkTrackNumber,
+    mkYear,
+ )
+
+newtype DisplayOptions = DisplayOptions
+    {doFile :: SomeBase File}
+    deriving (Show)
+
+data SetOrRemove a = Set a | Remove
+    deriving (Show)
+
+data EditOptions = EditOptions
+    { eoFile :: !(SomeBase File)
+    , eoTitle :: !(Maybe Title)
+    , eoArtist :: !(Maybe Artist)
+    , eoAlbum :: !(Maybe Album)
+    , eoGenre :: !(Maybe Genre)
+    , eoYear :: !(Maybe (SetOrRemove Year))
+    , eoTrack :: !(Maybe (SetOrRemove TrackNumber))
+    }
+    deriving (Show)
+
+data Options = Display DisplayOptions | Edit EditOptions
+    deriving (Show)
+
+optionsInfo :: ParserInfo Options
+optionsInfo = info (optionsP <**> helper) idm
+
+displayOptionsP :: Parser DisplayOptions
+displayOptionsP =
+    DisplayOptions
+        <$> argument
+            (maybeReader parseSomeFile)
+            (metavar "FILE")
+
+editOptionsP :: Parser EditOptions
+editOptionsP =
+    EditOptions
+        <$> argument
+            (maybeReader parseSomeFile)
+            (metavar "FILE")
+        <*> optional
+            ( strOption
+                ( long "title"
+                    <> metavar "TITLE"
+                    <> help "Set the title"
+                )
+            )
+        <*> optional
+            ( strOption
+                ( long "artist"
+                    <> metavar "ARTIST"
+                    <> help "Set the artist"
+                )
+            )
+        <*> optional
+            ( strOption
+                ( long "album"
+                    <> metavar "ALBUM"
+                    <> help "Set the album"
+                )
+            )
+        <*> optional
+            ( strOption
+                ( long "genre"
+                    <> metavar "GENRE"
+                    <> help "Set the genre"
+                )
+            )
+        <*> optional
+            ( option
+                (maybeReader strToYear)
+                ( long "year"
+                    <> metavar "YEAR"
+                    <> help "Set the year"
+                )
+                <|> flag'
+                    Remove
+                    ( long "noyear"
+                        <> help "Unset the year"
+                    )
+            )
+        <*> optional
+            ( option
+                (maybeReader strToTrackNumber)
+                ( long "track"
+                    <> metavar "TRACK"
+                    <> help "Set the track number"
+                )
+                <|> flag'
+                    Remove
+                    ( long "notrack"
+                        <> help "Unset the track"
+                    )
+            )
+  where
+    strToYear :: String -> Maybe (SetOrRemove Year)
+    strToYear = fmap Set . mkYear <=< readMaybe
+    strToTrackNumber :: String -> Maybe (SetOrRemove TrackNumber)
+    strToTrackNumber = fmap Set . mkTrackNumber <=< readMaybe
+
+optionsP :: Parser Options
+optionsP =
+    hsubparser
+        ( command
+            "display"
+            (info (Display <$> displayOptionsP) (progDesc "Show tags"))
+            <> command
+                "edit"
+                (info (Edit <$> editOptionsP) (progDesc "Edit tags"))
+        )
