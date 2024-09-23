310ef09c734bf7c98a3ee7797ccb55b99ffb7f6b
jecaro <jeancharles.quillet@gmail.com>
2024-09-23 20:56:29 +0200

    Bump nixpkgs to 24.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
   # description = "A Hello World in Haskell with a dependency and a devShell";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
@@ -31,8 +31,6 @@
             ghcid
             cabal-install
           ];
-          # Change the prompt to show that you are in a devShell
-          shellHook = "export PS1='\\[\\e[1;34m\\]dev > \\[\\e[0m\\]'";
         });
     };
 }
